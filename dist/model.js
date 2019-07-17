653574640c7864bb0899054199777f3cd11aefb5
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2019-07-17 12:46:27 +0300

    Describing your code with types: Learning TypeScript

--- dist/model.js
+++ dist/model.js
@@ -3,7 +3,9 @@ var inventoryType = "furniture";
 var trackingNumber = "FD123455";
 var createDate = new Date();
 var originalCost = 425;
-function getInventoryItem(trackingNumber) {}
+function getInventoryItem(trackingNumber) {
+  return null;
+}
 function saveInventoryItem(item) {}
 var inventoryItem = getInventoryItem(trackingNumber);
 inventoryItem.createDate = new Date();
